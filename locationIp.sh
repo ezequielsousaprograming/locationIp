471b125b33671cccb60cec9d4c53a891a9a37715
Ezequiel Sousa <138885922+ezequielwhaissousa@users.noreply.github.com>
2023-07-07 11:12:24 -0300

    add location.sh
    Script executável.

---
+++ locationIp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+COMMAND=/usr/bin/wget '\-qO' ipinfo.io/ip;
+
+if [ -c $COMMAND  ]
+then
+	echo 'Buscando dados em https://ipinfo.io'
+	sleep 2
+	$COMMAND
+else
+	echo 'Erro ao buscar Informações!'
+fi
