9e6fa278e4ecca3473cd316ed17281953f6b1998
Ezequiel Sousa <138885922+ezequielwhaissousa@users.noreply.github.com>
2023-07-31 11:27:04 -0300

    Update Shell Script

--- locationIp.sh
+++ locationIp.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-echo 'Banco Dados A Consultar: '; read DATABASE
+echo 'Versão do Terminal Bash: '; echo $BASH_VERSION
+echo 'Provedor de Consulta ( Ex: ipinfo.io: ) '; read DATABASE
 
-locationIp() {
-	COMMAND=$(/usr/bin/wget -qO- $DATABASE)
+locationIp()
+{
+	# Provedores Aceitos - Banco Dados
+	URL='ipinfo.io ipconfig.io'
+	# Busca Informações Servidor Com Wget
+	PROVIDER=$(/usr/bin/wget -qO- $DATABASE)
+	echo 'Informações de Protocolo de Internet e Localização: '	$PROVIDER
+	# Verifica Provedor Informado
+	if [ $URL ];
+	then
+		echo 'Provedor de Busca Não Encontrado!'
+		echo 'Provedores de Buscas Aceitos: ipinfo.io, ipconfig.io'
+	fi
 }
 
-if ! [ 'https://'  ] || [ 'http://' ] || [ ' ' ]
-then
-	echo 'Digite o endereço seguido do protocolo https ou http corretamente'
-else
-
-	if [ locationIp ]
-	then
-		echo 'Buscando dados em: '$DATABASE
-		locationIp
-	else
-		echo 'Erro ao buscar Informações!'
-		echo 'Não conseguimos encontrar seu endereço IP.'
-	fi
-
-fi
+# Executa a Função
+locationIp
