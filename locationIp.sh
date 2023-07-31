5238fc13736bd09bb91dad31fabd81a1da8772f1
Ezequiel Sousa <138885922+ezequielwhaissousa@users.noreply.github.com>
2023-07-31 15:04:51 -0300

    Update locationIp.sh

--- locationIp.sh
+++ locationIp.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-echo 'Versão do Terminal Bash: '; echo $BASH_VERSION
-echo 'Provedor de Consulta ( Ex: ipinfo.io: ) '; read DATABASE
+echo -n 'Versão do Terminal Bash: '; echo $BASH_VERSION
+echo -n 'Provedor de Consulta ( Ex: ipinfo.io, ipconfig.io: ) '; read DATABASE
 
 locationIp()
 {
 	# Provedores Aceitos - Banco Dados
-	URL='ipinfo.io ipconfig.io'
+	URL=('ipinfo.io' 'ipconfig.io')
 	# Busca Informações Servidor Com Wget
 	PROVIDER=$(/usr/bin/wget -qO- $DATABASE)
 	echo 'Informações de Protocolo de Internet e Localização: '	$PROVIDER
-	# Verifica Provedor Informado
-	if [ $URL ];
-	then
-		echo 'Provedor de Busca Não Encontrado!'
-		echo 'Provedores de Buscas Aceitos: ipinfo.io, ipconfig.io'
-	fi
-}
 
-# Executa a Função
-locationIp
+#	echo -n 'Selecione o tipo de Informação a Filtrar: 1 2 3'
+}
+# Verifica Provedor Digitado Pelo Usuário. Se o Provedor estiver no Banco de Dados Armazenados a Função é Executada. O Contrário é Listado alguns Provedores Aceitos.
+if [[ $DATABASE != ${URL[0]} && $PROVIDER != ${URL[1]} ]];
+then
+	echo 'Provedores de Buscas Aceitos: ipinfo.io, ipconfig.io'
+else
+	echo 'Executando Busca...'; locationIp
+fi
