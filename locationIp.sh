7520ef45aa9a90d0c6f51f14b7d1c77c7fc2a6ac
Ezequiel Sousa <138885922+ezequielwhaissousa@users.noreply.github.com>
2023-07-31 16:08:51 -0300

    Update locationIp.sh
    Reorganizando Instruções Condicionais.

--- locationIp.sh
+++ locationIp.sh
@@ -18,17 +18,18 @@ locationIp()
 }
 
 # Verifica Provedor Digitado Pelo Usuário. Se o Provedor estiver no Banco de Dados Armazenados a Função é Executada. O Contrário é Listado alguns Provedores Aceitos.
-if [[ $DATABASE != 'ipinfo.io' && $DATABASE != 'ipconfig.io' ]];
+if [[ $DATABASE != 'ipinfo.io' && $DATABASE != 'ipconfig.io' && $DATABASE != 'ifconfig.io' ]];
 then
 	echo 'Provedores de Buscas Aceitos: ipinfo.io, ipconfig.io, ifconfig.io'
 else
 	echo 'Executando Busca...'; locationIp
-fi
-# Cria Arquivo HTML e executa o Browser Firefox Exibe as Informações de Ip e Localização, caso a opção de servidor de busca será ifconfig.io. Necessário Instalação do Firefox.
-if [[ $DATABASE = 'ifconfig.io' ]];
-then
-	read -p 'Criando Arquivo HTML. Tecle Enter Para Confirmação?'
-	wget -qO- $DATABASE >> Ifconfig.html
-	sleep 2
-	firefox Ifconfig.html
+
+	# Cria Arquivo HTML e executa o Browser Firefox Exibe as Informações de Ip e Localização, caso a opção de servidor de busca será ifconfig.io. Necessário Instalação do Firefox.
+	if [[ $DATABASE = 'ifconfig.io' ]];
+	then
+		read -p 'Criando Arquivo HTML. Tecle Enter Para Confirmação?'
+		wget -qO- $DATABASE >> Ifconfig.html
+		sleep 2
+		firefox Ifconfig.html
+	fi
 fi
